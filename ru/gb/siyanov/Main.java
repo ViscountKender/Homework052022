3cdf9e8dc21bc7bde69aaeed810121a3578880d1
Viscount <siyanov.d@sar.garm.ru>
2022-05-29 13:50:23 +0400

    Создал массив участников и препятствий и 2  метода

--- ru/gb/siyanov/Main.java
+++ ru/gb/siyanov/Main.java
@@ -2,11 +2,6 @@ package ru.gb.siyanov;
 
 public class Main {
     public static void main(String[] args) {
-        int height;
-        int length;
-//        Members [] arrayMemb = new Members[3];
-//        arrayMenb[0] = new Members()
-
 
 
 
--- ru/gb/siyanov/Robot.java
+++
@@ -1,5 +0,0 @@
-package ru.gb.siyanov;
-
-public class Robot {
-
-}
